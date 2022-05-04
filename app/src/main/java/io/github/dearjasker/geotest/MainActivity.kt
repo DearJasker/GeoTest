90338688cee408963269b26e8ce35388c8042267
dearjasker <linyike2019@163.com>
2022-05-04 16:27:32 +0800

    GeoTest first commit

---
+++ app/src/main/java/io/github/dearjasker/geotest/MainActivity.kt
@@ -0,0 +1,40 @@
+package io.github.dearjasker.geotest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener{ view: View ->
+            // Users click the button
+            Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT
+            )
+                .show()
+        }
+
+        falseButton.setOnClickListener{ view: View ->
+            // Users click the button
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT,
+            )
+                .show()
+        }
+    }
+}
